5bb26624538b48626f2fae8f928d01d9c7e6d36a
nickdavis19 <nid34@pitt.edu>
2017-10-31 23:14:00 -0400

    local sensor functionality
    reads, compiles, and publishes the local sensor data

--- robot/LocalSensorData.py
+++ robot/LocalSensorData.py
@@ -6,12 +6,16 @@ import rospy
 import math
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from smbus2 import SMBus
+import time
 
-colorImage = Image()
-isColorImageReady = False;
+class SensorData():
+    red = 0
+    green = 0
+    blue = 0
 
 def main():
-    global colorImage, isColorImageReady
+    global data
     
 	########################################################
 	#Initialize the node, any subscribers and any publishers
@@ -25,27 +29,41 @@ def main():
 	#For data that would crash the program if it was not
 	#	ready yet
 	########################################################
-    while not isColorImageReady:
-        pass
-
-    while not rospy.is_shutdown():
-        try:
-            color_image = bridge.imgmsg_to_cv2(colorImage, "bgr8")
-        except CvBridgeError, e:
-            print e
-            print "colorImage"
 		
-		########################################################
-		#All code for processing data/algorithm goes here
-		########################################################
-        
-		
-		########################################################
-		#Publish data here
-		########################################################
-        pub.publish(imageMessage)
-        
+	########################################################
+	#All code for processing data/algorithm goes here
+	########################################################
+    data = SensorData()
+    sensorAddr =  0x44 ##address of the light sensor
+    i2c = SMBus(1) ##initialize SMBus object using the bus number
+    i2c.write_byte_data(0x44, 1, 0xD) ##configures the temperature sensor
 
+    num = 15
+    temp = 0
+
+    while(temp < num):
+        ##reads and compiles the green data
+        greenLow = i2c.read_byte_data(0x44, 9)
+        greenHigh = i2c.read_byte_data(0x44, 10)
+        data.green = redHigh<<8 | redLow
+
+        ##reads and compiles the red data
+        redLow = i2c.read_byte_data(0x44, 11)
+        redHigh = i2c.read_byte_data(0x44, 12)
+        data.red = redHigh<<8 | redLow
+
+        ##reads and compiles the blue data
+        blueLow = i2c.read_byte_data(0x44, 13)
+        blueHigh = i2c.read_byte_data(0x44, 14)
+        data.blue = redHigh<<8 | redLow
+		
+	    ########################################################
+	    #Publish data here
+	    #########################################################
+        pub.publish(data)
+
+        temp = temp + 1
+        time.sleep(1)
 
 
  
