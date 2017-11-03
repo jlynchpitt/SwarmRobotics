3c5c7ddb6aca53038d5c83750b6a9a13b9e3671c
nickdavis19 <nid34@pitt.edu>
2017-11-03 00:02:59 -0400

    local and global sensor nodes
    Added functionality for getting and publishing local data.
    Added functionality for keeping track of global maximum and prints to
    terminal.

--- centralHub/GlobalSensorDataProcessing.py
+++ centralHub/GlobalSensorDataProcessing.py
@@ -6,10 +6,11 @@ import rospy
 import math
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from collections import deque
 
-colorImage = Image()
-isColorImageReady = False;
-
+global queue, max
+queue = deque() ##deque uses FIFO, queue.pop(0) will pop the first element, queue.append() will append to the end
+max = 0
 ########################################################
 #Callback functions for each subscriber
 #	+ any other custom functions needed
@@ -18,23 +19,21 @@ isColorImageReady = False;
 #	processing in the main loop
 ########################################################
 def updateSensorData(data):
-    global colorImage, isColorImageReady, ratio
 	#May be best to store data in a buffer to deal with lots of data
 	#	coming in at once from multiple robots
 	#Or possibly have each robot publish to a different topic
 	#	and have multiple subscibers/callback functions
-    colorImage = data
-    isColorImageReady = True
+
+    queue.append(data)
 
 def main():
-    global colorImage, isColorImageReady
     
 	########################################################
 	#Initialize the node, any subscribers and any publishers
 	########################################################
     rospy.init_node('global_sensor_data_processing_node', anonymous=True)
     rospy.Subscriber("/local_sensor_data", Image, updateSensorData, queue_size=10)
-	imPub = rospy.Publisher('global_sensor_data', Image, queue_size=10)
+	globalPub = rospy.Publisher('global_sensor_data', Image, queue_size=10)
 	
 	########################################################
 	#Wait here for any data that needs to be ready
@@ -54,12 +53,15 @@ def main():
 		########################################################
 		#All code for processing data/algorithm goes here
 		########################################################
-        
+        current = queue.pop(0)
+        if(current > max):
+            max = current
+            print("Current max is " + max)
 		
 		########################################################
 		#Publish data here
 		########################################################
-        imPub.publish(imageMessage)
+        globalPub.publish(max)
         
 
 
