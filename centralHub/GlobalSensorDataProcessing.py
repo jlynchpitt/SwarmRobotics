703bfe3ece458dba249722a247d1643fbcfbac5d
nickdavis19 <nid34@pitt.edu>
2017-11-28 00:04:02 -0500

    various fixes, preparing for PSO

--- centralHub/GlobalSensorDataProcessing.py
+++ centralHub/GlobalSensorDataProcessing.py
@@ -15,11 +15,13 @@ currentRed = 0
 currentGreen = 0
 currentBlue = 0
 currentMax = 0
+
 theData = SensorData()
 theData.robotID = 1
 theData.red = 0
 theData.green = 0
 theData.blue = 0
+
 
 
 ########################################################
@@ -37,6 +39,7 @@ def updateSensorData(data):
     	#   and have multiple subscibers/callback functions
     	#queue.append(data)
 	theData = data
+
 def main():
     global queue, currentRed, currentGreen, currentBlue, currentMax, cmdVel, theData 
     ########################################################
@@ -44,7 +47,8 @@ def main():
     ########################################################
     rospy.init_node('global_sensor_data_processing_node', anonymous=True)
     rospy.Subscriber("/local_sensor_data", SensorData, updateSensorData, queue_size=10)
-    globalPub = rospy.Publisher('global_sensor_data', SensorData, queue_size=10)
+    rospy.Subscriber("/robot_location", RobotLocation, updateRobotLocation, queue_size=10)
+    sensorPub = rospy.Publisher('global_sensor_data', SensorData, queue_size=10)
     
     ########################################################
     #Wait here for any data that needs to be ready
@@ -70,7 +74,7 @@ def main():
         ########################################################
         #Publish data here
         ########################################################
-        globalPub.publish(theData) ##only publishing the global max value currently, needs to include the coordinate values
+        sensorPub.publish(theData)
         
 
 
