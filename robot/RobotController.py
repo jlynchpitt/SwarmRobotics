a705a7d82c4d9f1a9b8bff01de004daa8391d3dc
joe lynch <joelynch523@gmail.com>
2017-11-29 18:49:06 +0100

    Fix compile errors

--- robot/RobotController.py
+++ robot/RobotController.py
@@ -18,13 +18,12 @@
 #   wheel speeds
 ########################################################
 
-
 import roslib
 #roslib.load_manifest('rosopencv')
 import sys
 import rospy
 import math
-from swarm.msg import RobotVelocity, WheelSpeeds
+from swarm.msg import RobotVelocity, WheelSpeeds, RobotLocationList, RobotLocation
 
 cmdVel = RobotVelocity()
 cmdVel.x = 0
@@ -32,6 +31,10 @@ cmdVel.y = 0
 wheelSpeed = WheelSpeeds()
 wheelSpeed.rightWheel = 0
 wheelSpeed.leftWheel = 0
+locationList = RobotLocationList()
+location = RobotLocation()
+
+ROBOT_ID = 2
 
 ########################################################
 #Callback functions for each subscriber
@@ -45,12 +48,11 @@ def updateCommandedMovement(data):
     cmdVel = data
     
 def updateLocation(data):
-    global colorImage, isColorImageReady, ratio
-    colorImage = data
-    isColorImageReady = True
+    global locationList
+    locationList = data
 
 def main():
-    global cmdVel, wheelSpeed
+    global cmdVel, wheelSpeed, pid, locationList, location
     
     ########################################################
     #Initialize the node, any subscribers and any publishers
@@ -58,7 +60,7 @@ def main():
     ########################################################
     rospy.init_node('robot_controller_node', anonymous=True)
     rospy.Subscriber("/commanded_movement", RobotVelocity, updateCommandedMovement, queue_size=10)
-    #rospy.Subscriber("/location_data", Image, updateLocation, queue_size=10)
+    rospy.Subscriber("/location_data", RobotLocationList, updateLocation, queue_size=10)
     pub = rospy.Publisher('robot_commands', WheelSpeeds, queue_size=10)
     
     ########################################################
@@ -74,30 +76,78 @@ def main():
         # 1. Calculate vector magnitude + angle from XY velocities
         # 2. Calculate desired wheel speed (vMagnitude) 
         #       (identical wheel speed for each wheel if robot was moving forward in a straight line)
-	vMagnitude = math.sqrt(math.pow(cmdVel.x,2) + math.pow(cmdVel.y,2))
+        vMagnitude = math.sqrt(math.pow(cmdVel.x,2) + math.pow(cmdVel.y,2))
         
         #limit vMagnitude to 0-100 since wheel speeds are a percentage of max speed
         if vMagnitude > 100:
             vMagnitude = 100
         
-	print("cmd x: " + str(cmdVel.x) + " cmd y: " + str(cmdVel.y))
-	print("velocity magnitude: " + str(vMagnitude))
+	    print("cmd x: " + str(cmdVel.x) + " cmd y: " + str(cmdVel.y))
+	    print("velocity magnitude: " + str(vMagnitude))
 
         #TODO: Confirm this calculation is correct
         #If python 2.7 may do integer division for y/x
-	tanDivAmount = cmdVel.y
-	if cmdVel.x != 0:
-		tanDivAmount = cmdVel.y/cmdVel.x
-        vAngle = math.degrees(math.atan(tanDivAmount))
+	    tanDivAmount = cmdVel.y
+	    if cmdVel.x != 0:
+		    tanDivAmount = cmdVel.y/cmdVel.x
+            vAngle = math.degrees(math.atan(tanDivAmount))
+        elif cmdVel.y >= 0:
+            vAngle = 90
+        else:
+            vAngle = -90
         
         # 3. Read in current robot actual angle from location data
-        # 4. Use PID to calculate difference in wheel speeds to turn 
-        #       robot to desired angle
+        for i in range (0,locationList.numRobots):
+            if(locationList.robotList[i].robotID == ROBOT_ID):
+                location = locationList.robotList[i]
+                print("Found location for green robot")
+                break
+                
+        # 4. Calculate difference in wheel speeds to turn robot to desired angle
+        #How much to turn and what direction
+        angleToTurn = 0
+        clockWise = True
+        
+        currentAngle = location.angle
+        angleDiff = vAngle - currentAngle
+        angleDiffABS = math.fabs(angleDiff)
+        
+        wheelDiff = 0
+        
+        DEADZONE = 5 #+- angle to not turn anymore
+        if angleDiffABS < DEADZONE or angleDiffABS > (360-DEADZONE):
+            if angleDiff >= 0 and angleDiff <= 180:
+                angleToTurn = angleDiff
+                clockWise = False
+            elif angleDiff <= 0 and angleDiff >= -180:
+                angleToTurn = angleDiffABS
+                clockWise = True
+            elif angleDiff > 180 and angleDiff < 360:
+                angleToTurn = 360 - angleDiff
+                clockWise = True
+            else: #-181 ->> -360
+                angleToTurn = 360 + angleDiff
+                clockWise = False
+                
+            if angleToTurn < 45:
+                wheelDiff = 5
+            elif angleToTurn < 90:
+                wheelDiff = 10
+            else:
+                wheelDiff = 15
+            
+        # clockwise increase left wheel speed
+        # counterclockwise - increase right wheel speed
+        multiplier = -1
+        if(clockWise == True):
+            multiplier = 1
+
+        vMagnitude = 0 #TODO: This is temporary for testing
         
         # 5. Publish calculated wheel speeds
-        wheelSpeed.rightWheel = vMagnitude
-        wheelSpeed.leftWheel = vMagnitude
-        
+        wheelSpeed.rightWheel = vMagnitude + wheelDiff * multiplier * -1
+        wheelSpeed.leftWheel = vMagnitude + wheelDiff * multiplier
+
         pub.publish(wheelSpeed)
  
 if __name__ == '__main__':
