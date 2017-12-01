7aeb2272d1fd1c233fdbd43982e77ba325529101
jlynchpitt <joelynch523@gmail.com>
2017-11-30 20:55:41 -0500

    fixed publishing data type

--- robot/LocalSensorData.py
+++ robot/LocalSensorData.py
@@ -22,7 +22,7 @@ def main():
 	#Note no subscribers needed for this node
 	########################################################
     rospy.init_node('local_sensor_data_node', anonymous=True)
-	pub = rospy.Publisher('local_sensor_data', Image, queue_size=10)
+	pub = rospy.Publisher('local_sensor_data', SensorData, queue_size=10)
 	
 	########################################################
 	#Wait here for any data that needs to be ready
@@ -30,11 +30,7 @@ def main():
 	#	ready yet
 	########################################################
     while not rospy.is_shutdown():
-        try:
-            color_image = bridge.imgmsg_to_cv2(colorImage, "bgr8")
-        except CvBridgeError, e:
-            print e
-            print "colorImage"
+
 	########################################################
 	#All code for processing data/algorithm goes here
 	########################################################
