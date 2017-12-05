c12185c4c65e6361a1a07e642d27f0700c797bd3
Joe Lynch <joelynch523@gmail.com>
2017-12-05 16:33:31 -0500

    Add triangle recognition to obstacle avoidance

--- robot/ObstacleAvoidance.py
+++ robot/ObstacleAvoidance.py
@@ -166,7 +166,7 @@ def main():
                 if(newLocationList.robotList[i].robotID != ROBOT_ID):
                     P = Point(newLocationList.robotList[i].x, newLocationList.robotList[i].y)
                     print("Checking robot: " + str(newLocationList.robotList[i].robotID))
-                    if(PointInTriangle(P, A, B, C)):
+                    if(PointInTriangleBT(P, A, B, C) == True):
                         print("Obstacle in sight")
                     break
         else:
@@ -199,6 +199,23 @@ def PointInTriangle(pt, v1, v2, v3):
     b3 = sign(pt, v3, v1) < float(0.0)
 
     return ((b1 == b2) and (b2 == b3))
+
+def PointInTriangleBT(P, A, B, C):
+    v0 = Point(C.x - A.x, C.y - A.y)
+    v1 = Point(B.x - A.x, B.y - A.y)
+    v2 = Point(P.x - A.x, P.y - A.y)
+    
+    dot00 = dot(v0, v0)
+    dot01 = dot(v0, v1)
+    dot02 = dot(v0, v2)
+    dot11 = dot(v1, v1)
+    dot12 = dot(v1, v2)
+    
+    invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
+    u = (dot11 * dot02 - dot01 * dot12) * invDenom
+    v = (dot00 * dot12 - dot01 * dot02) * invDenom
+    
+    return (u >= 0) and (v >= 0) and ((u+v) < 1)
        
 class Point:
     """ Point class represents and manipulates x,y coords. """
